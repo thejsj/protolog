2695642fec56a81f35f762a9272bdc72af35e12f
Loring Dodge <loringdodge@yahoo.com>
2015-04-13 22:37:08 -0700

    Add color, bold, underline, and background methods

--- examples/node/colors.js
+++ examples/node/colors.js
@@ -1,5 +1,49 @@
 var l = require('../../')();
 
+// TEXT COLOR - color(), bold(), underline()
+
+  // Default: 'black'
+  // Available colors: 'black', 'red', 'green', 'yellow', 'purple', 'pink', 'cyan', 'grey'
+  var str = 'hello';
+
+  str.log.color('black');
+  str.log.color('red');
+  str.log.color('green');
+
+  str.log.bold('yellow');
+  str.log.bold('purple');
+  str.log.bold('pink');
+
+  str.log.underline('cyan');
+  str.log.underline('grey');
+
+  // LowerCase: Not required
+  str.log.color('Yellow');
+  str.log.bold('PuRpLe');
+  str.log.underline('GREEN');
+
+// BACKGROUND COLOR - background()
+
+  // Default: 'white'
+  // Available colors: 'black', 'red', 'green', 'yellow', 'purple', 'pink', 'cyan', 'grey'
+  var str = 'hello';
+
+  str.log.background('black');
+  str.log.background('red');
+  str.log.background('green');
+  str.log.background('yellow');
+  str.log.background('purple');
+  str.log.background('pink');
+  str.log.background('cyan');
+  str.log.background('grey');
+
+  // LowerCase: Not required
+  str.log.background('Yellow');
+  str.log.background('PuRpLe');
+  str.log.background('GREEN');
+
+// ************************
+
 // Log strings
 var str = 'hello';
 str.log('red');
