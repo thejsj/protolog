08c9fe05934ac2ab5cfef90e071574d2071a2d00
thejsj <jorge.silva@thejsj.com>
2015-04-14 08:44:34 -0700

    Finish texts

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var consoleStub = require('./console-overwrite');
 var should = require('should');
+var expect = require('chai').expect;
 var l = require('../index')( {
   loggingFunction: consoleStub,
   appendToPrototype: false
@@ -34,26 +35,30 @@ describe('Log function', function () {
 describe('Global Prototype Function', function () {
 
   it('should not set the global `.log` method if indicated in the option', function () {
-    should()'hello'.log;
+    expect('hello'.log).to.equal(undefined);
   });
 
   it('should console.log from the prototype `.log` method', function () {
     var l = require('../index')( {
       loggingFunction: consoleStub,
-      appendToPrototype: false
+      appendToPrototype: true
     });
     'hello'.log();
     consoleStub.getLastLog().should.equal('hello');
     'not-hello'.log();
     consoleStub.getLastLog().should.not.equal('hello');
-    l({ value: 5 });
-    consoleStub.getLastLog().should.eql({ value: 5 });
-    l([1, 2, 3]);
-    consoleStub.getLastLog().should.eql([1, 2, 3]);
   });
 
   it('should set the propertyName of the method according to the `propertyName` option', function () {
-
+    var l = require('../index')( {
+      loggingFunction: consoleStub,
+      appendToPrototype: true,
+      propertyName: 'print'
+    });
+    'hello'.print();
+    consoleStub.getLastLog().should.equal('hello');
+    'not-hello'.print();
+    consoleStub.getLastLog().should.not.equal('hello');
   });
 
 
